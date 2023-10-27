ee14eb8a0a1cab0ca3365dc844d76cb307bb7e09
sarthak9096 <117964919+sarthak9096@users.noreply.github.com>
2023-10-27 14:54:22 +0530

    Update views.py

--- employee/views.py
+++ employee/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render
 from django.shortcuts import render, redirect  
 from employee.forms import EmployeeForm  
 from employee.models import Employee 
-import time
 def emp(request):  
     if request.method == "POST":  
         form = EmployeeForm(request.POST)  

8c8f4a3a49c4bfe76ae9d1e313445a4ac7e4ba69
sarthak9096 <117964919+sarthak9096@users.noreply.github.com>
2023-11-01 15:12:06 +0530

    Update views.py

--- employee/views.py
+++ employee/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render
 from django.shortcuts import render, redirect  
 from employee.forms import EmployeeForm  
 from employee.models import Employee 
-import time
 def emp(request):  
     if request.method == "POST":  
         form = EmployeeForm(request.POST)  
@@ -31,4 +30,4 @@ def update(request, id):
 def destroy(request, id):  
     employee = Employee.objects.get(id=id)  
     employee.delete()  
-    return redirect("/show")
+    return redirect("/emp")
